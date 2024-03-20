e41411b3d159ae9eae54281faf749650cd1a21ea
Logan MacAskill <logan@macaskill.com>
2024-03-19 18:36:33 -0700

    clean up and add branding

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const githubDark = require('./src/css/githubDark');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '6962 Technical Wiki',
-  tagline: '6962 Best Team',
+  tagline: 'Documentation for important FRC Software',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -41,19 +41,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -72,23 +67,17 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '6962 Technical Wiki',
         logo: {
-          alt: 'My Site Logo',
+          alt: '6962 Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docsSidebar',
             position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            label: 'Docs',
           },
         ],
       },
